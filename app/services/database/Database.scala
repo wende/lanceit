b614919a0d63c8b896f314ef7c03720d7d461239
Krzysztof Wende <iraasta@gmail.com>
2014-11-24 16:26:17 +0100

    ensure

--- app/services/database/Database.scala
+++ app/services/database/Database.scala
@@ -10,11 +10,11 @@ import play.api.libs.concurrent.Execution.Implicits._
 
 object Database {
 
-  def persons: JSONCollection = ttl(){ db.collection[JSONCollection]("persons")}
-  def feeds : JSONCollection = ttl(){ db.collection[JSONCollection]("feeds")}
+  val persons: JSONCollection = ttl(){ db.collection[JSONCollection]("persons")}
+  val feeds : JSONCollection = ttl(){ db.collection[JSONCollection]("feeds")}
 
 
-  def ttl( field : String = "Date")(collection : => JSONCollection) =
+  def ttl( field : String = "Date")(collection : => JSONCollection)  =
   {
     collection.indexesManager.ensure(Index(
       Seq((field, IndexType(BSONInteger(1)))), Some(field),
