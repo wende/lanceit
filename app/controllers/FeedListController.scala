4a3cf67b2c7db45760dd3f33896d4ca30b3aeba6
Krzysztof Wende <iraasta@gmail.com>
2014-11-24 15:54:39 +0100

    feeds json

--- app/controllers/FeedListController.scala
+++ app/controllers/FeedListController.scala
@@ -16,7 +16,7 @@ import scala.concurrent.Future
 object FeedListController extends Controller{
   def get = Action.async {
     Database.feeds.find( Json.obj() ).cursor[FeedItem].collect[List]().map {
-      feeds => Ok(Json.toJson(feeds))
+      feeds => Ok(Json.toJson( Json obj "feeds" ->feeds ))
     }
   }
   def add = Action.async(parse.json) { req =>
