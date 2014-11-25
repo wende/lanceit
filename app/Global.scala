8b416bd5f37a740f58a26af69ed8d04d601e6423
Krzysztof Wende <iraasta@gmail.com>
2014-11-25 18:24:37 +0100

    feeds

--- app/Global.scala
+++ app/Global.scala
@@ -3,7 +3,7 @@ import java.util.Calendar
 import models.FeedItem
 import play._
 import play.api.libs.json._
-import reactivemongo.bson.BSONDateTime
+import reactivemongo.bson.{BSONObjectID, BSONDateTime}
 import services.database.Database
 import models.JsonFormats._
 import play.api.libs.concurrent.Execution.Implicits._
@@ -15,7 +15,7 @@ class Global extends GlobalSettings {
   {
     val time = Calendar.getInstance().getTimeInMillis
     Logger.info("Server started and configured")
-    val item = FeedItem("iraasta","Title","Desc",time ,0,0,BSONDateTime(time + 1000*60*60*24*7))
+    val item = FeedItem(BSONObjectID.generate, "iraasta","Title","Desc",time ,0,0,BSONDateTime(time + 1000*60*60*24*7))
     Logger.info(Json.toJson(item).toString())
     Database.feeds.insert(item)
   }
