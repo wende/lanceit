21d0595b0817b378e614bc41192a3082cdee9b92
Haito <hajtosek@gmail.com>
2015-02-10 11:49:58 +0100

    Menus, contact

--- public/homepage/assets/js/map.js
+++ public/homepage/assets/js/map.js
@@ -3,15 +3,15 @@ jQuery(document).ready(function(){
 
     map = new GMaps({
         div: '#map',
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 51.4995167,
+        lng: -0.220892
     });
     map.addMarker({
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 51.4995167,
+        lng: -0.220892,
         title: 'Address',      
         infoWindow: {
-            content: '<h5 class="subtitle">Atom HQ</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
+            content: '<h5 class="subtitle">Neon Tree Solutions HQ</h5><p><span class="region">220 C Blythe Road, London</span><br><span class="postal-code">W14 0HH</span><br><span class="country-name">Anglia</span></p>'
         }
         
     });
